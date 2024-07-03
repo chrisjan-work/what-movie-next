b0797222fe84c1ac815124261cc915b60f6c71c3
Christiaan Janssen <113596768+chrisjan-work@users.noreply.github.com>
2024-07-03 14:56:40 +0200

    remove hello world cucumber feature

--- app/src/androidTest/assets/features/HelloWorld.feature
+++
@@ -1,5 +0,0 @@
-Feature: Hello World
-
-  Scenario: First Run
-    Given the app starts
-    Then the text "Hello Android!" is displayed
--- app/src/androidTest/java/com/lairofpixies/whatmovienext/test/MovieListStepDefs.kt
+++ app/src/androidTest/java/com/lairofpixies/whatmovienext/test/MovieListStepDefs.kt
@@ -1,24 +1,16 @@
 package com.lairofpixies.whatmovienext.test
 
 import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.onNodeWithText
 import dagger.hilt.android.testing.HiltAndroidTest
 import io.cucumber.java.en.Given
-import io.cucumber.java.en.Then
 
 @HiltAndroidTest
-class HelloWorldStepDefs(
+class MovieListStepDefs(
     private val composeRuleHolder: ComposeRuleHolder,
     private val scenarioHolder: ActivityScenarioHolder,
 ) : SemanticsNodeInteractionsProvider by composeRuleHolder.composeRule {
     @Given("the app starts")
     fun theAppStarts() {
         scenarioHolder.launch()
-    }
-
-    @Then("the text {string} is displayed")
-    fun theTextIsDisplayed(text: String) {
-        onNodeWithText(text).assertIsDisplayed()
     }
 }
