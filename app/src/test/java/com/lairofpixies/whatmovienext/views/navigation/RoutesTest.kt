e69ce8695049c38c6048358188b0d9d24e423158
Christiaan Janssen <113596768+chrisjan-work@users.noreply.github.com>
2024-07-27 09:08:46 +0200

    Add GPL license header to code

--- app/src/test/java/com/lairofpixies/whatmovienext/views/navigation/RoutesTest.kt
+++ app/src/test/java/com/lairofpixies/whatmovienext/views/navigation/RoutesTest.kt
@@ -1,3 +1,21 @@
+/*
+ * This file is part of What Movie Next.
+ *
+ * Copyright (C) 2024 Christiaan Janssen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.lairofpixies.whatmovienext.views.navigation
 
 import org.junit.Assert.assertEquals
